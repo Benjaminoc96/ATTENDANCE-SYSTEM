61552815c606108e863bd6b1979348c876bed54e
Catalyst-OTU <bismarkotu1995@gmail.com>
2023-02-10 16:49:00 +0000

    SoftDelete on Visitor Model

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Visitor extends Model
 {
+
+    use SoftDeletes;
     use HasFactory;
     protected $table = 'visitors';
     protected $fillable = ['name', 'contact', 'address', 'department', 'staff','purpose', 'visitor_type', 'log_type'];
